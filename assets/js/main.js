efca5b7a0d88f46e28b3e87dd2569ea9435dd838
Puneet Singh <puneet.singh@mojoauth.com>
2024-06-10 22:31:46 +0530

    Fix clock

--- assets/js/main.js
+++ assets/js/main.js
@@ -20,7 +20,7 @@
     /* ==========================================================================
        countdown timer
        ========================================================================== */
-     jQuery('#clock').countdown('2024/06/23',function(event){
+     jQuery('#clock').countdown('2024/06/23 09:00:00',function(event){
       var $this=jQuery(this).html(event.strftime(''
       +'<div class="time-entry days"><span>%-D</span> Days</div> '
       +'<div class="time-entry hours"><span>%H</span> Hours</div> '
